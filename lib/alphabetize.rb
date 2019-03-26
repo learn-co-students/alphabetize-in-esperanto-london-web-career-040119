8081382e5d58aa5d5526164eec24695a5d378cb0
Xabier Casan <xabier_abia@hotmail.com>
2019-03-26 22:40:51 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ require "pry"
 
 def alphabetize(arr)
   esperanto_alphabet=" abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by { |arr| arr.split('').collect{ |x| esperanto_alphabet.index(x)}}
+  arr.sort_by { |arr| arr.split('').collect { |x| esperanto_alphabet.index(x)}}
 end
