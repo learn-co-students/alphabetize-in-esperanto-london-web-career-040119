35bc1cb76aeb73a0370a94e84a11a9e237778335
Ay Has <ahashim983@yahoo.com>
2019-04-01 22:47:14 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(word_list)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sorter = alphabet.split("")
+  w = word_list.sort
+  return w.sort_by{|x| sorter.index(x[0])}
 end
+#puts alphabetize(["ŝab","ĉab","fab"])
